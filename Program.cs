fd080c06c19a213c0b757de2cf2ea545e6ebd9b7
WilfullMurder <81480788+WilfullMurder@users.noreply.github.com>
2021-06-29 11:11:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OddorEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Number;
+           
+
+            Console.WriteLine("Enter a Number: ");
+            Number = Console.ReadLine();
+            int result = Int32.Parse(Number);
+
+            if(result % 2 != 0) {
+
+                Console.WriteLine(result + " is odd");
+
+            }
+            else
+            {
+                Console.WriteLine(result + " is even");
+            }
+        }
+    }
+}
